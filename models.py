6c5be72383847ef2fbf6e0e4ccd4c2275892e68d
Wil Brito <wil.brito@icloud.com>
2025-01-23 12:33:55 +0100

    Realización de models.py empleado para la validación y estandarización de los datos(texto) pedido al modelom IA

---
+++ models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+"""
+Empleamos la librería pydantic para la validación y estandarización de datos 
+"""
+
+class PedirGeneTexto(BaseModel):
+    """
+    OBJ: Dotar de una estructura determinada al texto que se va a pedir, generado por el modelo IA
+    PRE: Configurar parámetros opcionales del modelo, como longitud máxima del texto generado, temperatura, y top-p.
+    """
+    prompt: str = Field(..., min_length=1 , description="Texto inicial")
+    longitud: int = Field(ge=50, le=500, description="Rango (número) de las palabras generadas")
+    temperatura: float = Field(ge=0.0, le=1.0, description="Aleatoriedad de la respuesta")
+    top_p: float = Field(ge=0.0, le=1.0, description="Alternativa a temperatura")
+
+
+class RespuestaGeneTexto(BaseModel):
+    """
+    OBJ: Recibir la respuesta generada por el modelo IA
+    """
+    prompt : str
+    texto_generado : str
