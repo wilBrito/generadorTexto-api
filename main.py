ea180e2086619c70f0cace60d99bcd6bf162dda3
Wil Brito <wil.brito@icloud.com>
2025-01-25 12:05:09 +0100

    Implementado sistema de logs para las solicitudes y respuestas

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI, HTTPException
 from models import RespuestaGeneTexto, PedirGeneTexto
 from database import guardar_respuesta, ver_historial
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import logging
+
+# Configuración del logger
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("Logger")
 
 app = FastAPI(title="Generador de Texto-Roams")
 
@@ -19,7 +25,11 @@ async def generar_texto(request: PedirGeneTexto):
     PRE: prompt no tiene que estar vacío
     """
     if not request.prompt.strip():
+        logger.error("El prompt está vacío Error!!!!")
         return {"prompt":request.prompt, "texto_generado": "El prompt está vacío Error!!!!"}
+    
+    #Logger Solicitud
+    logger.info(f"Solicitud: {request.prompt}")
 
     try:
         result = model.generate(
@@ -31,16 +41,18 @@ async def generar_texto(request: PedirGeneTexto):
             pad_token_id=tokenizer.eos_token_id
         )
 
-        #print(result)
         txt_generado = tokenizer.decode(result[0], skip_special_tokens=True)
-        #print(txt_generado)
-
+        
         #Guardar en la base de datos
         guardar_respuesta(request.prompt, txt_generado)
 
+        #Logger Respuesta
+        logger.info(f"Respuesta: {txt_generado}")
+
         return {"prompt":request.prompt, "texto_generado":txt_generado}
     except:
-        print("Error al ejecutar el modelo IA")
+        logger.error("Error al ejecutar el modelo IA")
+        #print("Error al ejecutar el modelo IA")
 
 #Método GET, extraer las consultas realizadas de la database
 @app.get("/historial")
